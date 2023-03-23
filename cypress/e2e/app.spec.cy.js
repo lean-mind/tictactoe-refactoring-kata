46c592da2189685306daa2e97020cae8e9f7b8e5
Maria Soria <maria.it3@gmail.com>
2023-03-23 09:09:43 +0000

    Add e2e test for player winner and loser

--- cypress/e2e/app.spec.cy.js
+++ cypress/e2e/app.spec.cy.js
@@ -1,7 +1,23 @@
-describe('in the tic tac toe game', () => {
-  it('the player should win', () => {
-    cy.visit('http://localhost:3000/')
-    cy.contains('Start Game').click()
-    cy.contains('Scoreboard')
-  })
+describe('In the tic tac toe game', () => {
+    it('the player should win', () => {
+        cy.visit('http://localhost:3000/')
+        cy.contains('Start Game').click()
+        cy.contains('Scoreboard')
+        cy.get('#ai_level').select('easy')
+        cy.get('#block_0').click()
+        cy.get('#block_4').click()
+        cy.get('#block_8').click()
+        cy.get('#playerstat1').contains('1')
+    })
+    it('the player should lose', () => {
+        cy.visit('http://localhost:3000/')
+        cy.contains('Start Game').click()
+        cy.contains('Scoreboard')
+        cy.get('#ai_level').select('easy')
+        cy.get('#block_1').click()
+        cy.get('#block_2').click()
+        cy.get('#block_5').click()
+        cy.get('#block_7').click()
+        cy.get('#computerstat1').contains('1')
+    })
 })
